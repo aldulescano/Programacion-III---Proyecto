a4c8548d8fefe0b9c81d611d08017e9bf86263b2
Gabuxto <gcarrionmayorga@udesa.edu.ar>
2022-09-01 15:47:37 -0300

    ver más v1

--- proyecto/src/screens/VerTodas/VerTodas.js
+++ proyecto/src/screens/VerTodas/VerTodas.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
 
 class VerTodas extends Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props);
         this.state={
+            value:''
         }
     }
 
-    render(){
-        return(
-            <React.Fragment>
-                <h1>Todas las películas</h1>
-            </React.Fragment>
-        )
+    evitarDefault(evento){
+        evento.preventDefault();
+        
     }
 
-}
+    obtenerDatos(evento){
+        this.setState({
+          value: evento.target.value,  
+        }, () => console.log(this.state.value))
+    }
 
+
+    render(){
+        return(
+            <form onSubmit={(evento)=>this.evitarDefault(evento)} className='mb-4'>
+                <input onChange={(evento)=>this.obtenerDatos(evento)} type="text" name="usuario" value={this.state.value} />
+                <button type="submit">Enviar</button>
+            </form>
+        )
+    }
+}
 
 export default VerTodas;
