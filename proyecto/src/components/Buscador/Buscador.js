f7eae730886047a9c6116fca53b362bf616505a0
aldulescano <lescanoa@udesa.edu.ar>
2022-09-06 09:23:44 -0300

    estructura barra buscadora

---
+++ proyecto/src/components/Buscador/Buscador.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './buscador.css'
+
+
+class Buscador extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+            </React.Fragment>
+        )
+    }
+};
+
+export default Buscador;
